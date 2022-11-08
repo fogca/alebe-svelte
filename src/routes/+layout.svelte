ea6d978cf867c2a9ef83e02dc8766666588c4cc3
one-tokyo <info@one.tokyo.jp>
2022-11-09 00:46:52 +0900

    1109_0046

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,9 +14,9 @@ import { afterNavigate, beforeNavigate } from '$app/navigation';
 </script>
   
 <svelte:head>
-  <link rel="stylesheet" href="../css/base.css">
-  <link rel="stylesheet" href="../css/main.css">
-  <link rel="stylesheet" href="../css/layout.css">
+  <link rel="stylesheet" href="../css/base.css?ver=1.0.1">
+  <link rel="stylesheet" href="../css/main.css?ver=1.0.1">
+  <link rel="stylesheet" href="../css/layout.css?ver=1.0.1">
   <link rel="stylesheet" href="https://use.typekit.net/frp6udl.css">
   <script>
     (function(d) {
